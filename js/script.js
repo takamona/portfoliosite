32997fc3dd7a0c2773e058f2515cf0964301155a
EC2 Default User <ec2-user@ip-172-31-26-40.ec2.internal>
2022-03-27 12:14:32 +0000

    ポートフォリオサイト

---
+++ js/script.js
@@ -0,0 +1,5 @@
+const button = document.getElementById('s1');
+            button.addEventListener('click', () => {
+               const sinki = document.getElementsByClassName('sinki')[0];
+               sinki.style.visibility = "visible";
+            });
