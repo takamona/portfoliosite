7c420e73bea27c693d49f5a401b2715a8a82e9dc
EC2 Default User <ec2-user@ip-172-31-26-40.ec2.internal>
2022-03-30 19:00:39 +0000

    ポートフォリオサイト修正

---
+++ js/comyunity_topics.js
@@ -0,0 +1,29 @@
+const btn = document.getElementsByClassName("reply")[0];
+btn.addEventListener("click",() => {
+const reply = document.getElementsByClassName("hidebutton")[0];
+reply.style.visibility="visible";
+});
+
+const btn2 = document.getElementsByClassName("reply")[1];
+btn2.addEventListener("click",() => {
+const reply2 = document.getElementsByClassName("hidebutton")[1];
+reply2.style.visibility="visible";
+});
+
+const btn3 = document.getElementsByClassName("reply")[2];
+btn3.addEventListener("click",() => {
+const reply3 = document.getElementsByClassName("hidebutton")[2];
+reply3.style.visibility="visible";
+});
+
+const btn4 = document.getElementsByClassName("reply")[3];
+btn4.addEventListener("click",() => {
+const reply4 = document.getElementsByClassName("hidebutton")[3];
+reply4.style.visibility="visible";
+});
+
+const btn5 = document.getElementsByClassName("reply")[4];
+btn5.addEventListener("click",() => {
+const reply5 = document.getElementsByClassName("hidebutton")[4];
+reply5.style.visibility="visible";
+});
